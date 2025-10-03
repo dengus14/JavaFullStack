c341038ff1a25948387955151871447896071870
dengus14 <ddgusev0318@gmail.com>
2025-10-03 15:30:30 -0500

    initial commit

---
+++ src/main/java/com/vault/DenisProject/DenisProjectApplication.java
@@ -0,0 +1,13 @@
+package com.vault.DenisProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenisProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DenisProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vault/DenisProject/DenisProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vault.DenisProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenisProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
