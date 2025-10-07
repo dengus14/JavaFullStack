c9f7c0ac6014953c09c64e2af14e053014c849e5
dengus14 <ddgusev0318@gmail.com>
2025-10-06 21:21:56 -0500

    last changes of the day

--- backend/src/main/java/com/vault/DenisProject/controllers/FileController.java
+++ backend/src/main/java/com/vault/DenisProject/controllers/FileController.java
@@ -2,12 +2,18 @@ package com.vault.DenisProject.controllers;
 
 import com.vault.DenisProject.dto.FileUpdateRequest;
 import com.vault.DenisProject.service.FileService;
+import jakarta.annotation.Resource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.vault.DenisProject.models.FileMetadata;
 import org.springframework.web.multipart.MultipartFile;
 
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 
@@ -53,4 +59,15 @@ public class FileController {
         return "File " + file.getOriginalFilename() + " uploaded successfully";
 
     }
+    @GetMapping("/download/{id}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable Long id) {
+        InputStream stream = fileService.downloadFile(id);
+        InputStreamResource resource = new InputStreamResource(stream);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=fileName")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body((Resource) resource);
+    }
+
 }
--- backend/src/main/java/com/vault/DenisProject/service/FileService.java
+++ backend/src/main/java/com/vault/DenisProject/service/FileService.java
@@ -1,6 +1,7 @@
 package com.vault.DenisProject.service;
 
 import com.vault.DenisProject.repository.FileRepository;
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -8,9 +9,13 @@ import com.vault.DenisProject.models.FileMetadata;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.S3Object;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -22,6 +27,7 @@ import java.util.concurrent.atomic.AtomicLong;
 public class FileService {
 
     private final FileRepository fileRepository;
+
     private final S3Client s3Client;
     @Value("${aws.s3.bucket}")
     private String bucketName;
@@ -95,6 +101,19 @@ public class FileService {
         metadata.setS3Url(fileUrl);
 
         fileRepository.save(metadata);
+    }
+
+    public InputStream downloadFile(Long id) {
+
+        FileMetadata ret =  fileRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("File with ID " + id + " not found"));
+
+        GetObjectRequest newRequest = GetObjectRequest.builder().bucket(bucketName).key(ret.getS3Url()).build();
+
+        try {
+            return s3Client.getObject(newRequest);
+        } catch (S3Exception e) {
+            throw new RuntimeException("Error downloading file from S3: " + e.awsErrorDetails().errorMessage(), e);
+        }
     }
 
 }
